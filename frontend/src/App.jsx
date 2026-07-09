b63f687df899a409f9417e59440f56aca6228889
UX SCOUTS <admin@uxscouts.com>
2026-07-09 15:49:12 +0000

    navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Users3 from './components/Users3';
 import Users2 from './components/Users2';
-import { Button } from 'reactstrap';
 import {
   Navbar,
   NavbarBrand,
@@ -9,27 +9,78 @@ import {
   Collapse,
   Nav,
   NavItem,
-  NavLink
+  NavLink,
+  Container
 } from 'reactstrap';
 
 
 const Home = () => <h2>🏠 Home Page</h2>;
 const About = () => <h2>ℹ️ About Page</h2>;
 
-
-function App() {
+function NavigationBar() {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
   return (
     <>
-    <div className="container mt-5">
-      <Button color="primary">Click Me</Button>
+     <Router>
+        <Navbar color="light" light expand="md" className="px-3 shadow-sm">
+          <NavbarBrand href="/">MyBrand</NavbarBrand>
+          <NavbarToggler onClick={toggle} />
+          <Collapse isOpen={isOpen} navbar>
+            <Nav className="ms-auto" navbar>
+              <NavItem>
+                <NavLink href="/">Home</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/about">About</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/users3">Users3</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+    </Router>
+    <div>
+        <div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/users3" element={<Users3 />} />
+          </Routes>
+        </div>
     </div>
+  </>
+  );
+}
+
+
+
+
+function App() {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+  return (
+    <>
     <Router>
       <div>
-        <nav>
-          <Link to="/" style={{ marginRight: '10px' }}>Home</Link>
-          <Link to="/about">About</Link>
-          <Link to="/users2">Users2</Link>
-        </nav>
+        <Navbar color="light" light expand="md" className="px-3 shadow-sm">
+          <NavbarBrand href="/">MyBrand</NavbarBrand>
+          <NavbarToggler onClick={toggle} />
+          <Collapse isOpen={isOpen} navbar>
+          <Nav className="ms-auto" navbar>
+           <NavItem>
+           <NavLink href="/">Home</NavLink>
+          </NavItem>
+          <NavItem>
+           <NavLink href="/about">About</NavLink>
+          </NavItem>
+           <NavItem>
+          <NavLink href="/users2">Users2</NavLink>
+          </NavItem>
+          </Nav>
+          </Collapse>
+        </Navbar>
         <div>
           <Routes>
             <Route path="/" element={<Home />} />
