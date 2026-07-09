bc0ddb0f448f0945ba50cb36e896178571012eb3
UX SCOUTS <admin@uxscouts.com>
2026-07-09 14:36:40 +0000

    first commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,46 @@
-import { useState } from 'react'
-import Users3 from './components/Users3';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Users2 from './components/Users2';
-import './App.css'
+import { Button } from 'reactstrap';
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  Collapse,
+  Nav,
+  NavItem,
+  NavLink
+} from 'reactstrap';
+
+
+const Home = () => <h2>🏠 Home Page</h2>;
+const About = () => <h2>ℹ️ About Page</h2>;
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <p>Something</p>
-      <Users3/>
-      <Users2/>
+    <div className="container mt-5">
+      <Button color="primary">Click Me</Button>
+    </div>
+    <Router>
+      <div>
+        <nav>
+          <Link to="/" style={{ marginRight: '10px' }}>Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/users2">Users2</Link>
+        </nav>
+        <div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/users2" element={<Users2 />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
