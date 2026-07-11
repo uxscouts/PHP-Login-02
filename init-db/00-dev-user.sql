857584c6e627ae017872eafe22168496c797d693
UX SCOUTS <admin@uxscouts.com>
2026-07-11 15:50:30 +0000

    added tomato220 database and gave dev_user permission

--- init-db/00-dev-user.sql
+++ init-db/00-dev-user.sql
@@ -3,3 +3,7 @@ CREATE USER IF NOT EXISTS 'dev_user'@'localhost' IDENTIFIED BY 'dev_password';
 
 -- If the user already exists, update their password just to be sure
 ALTER USER 'dev_user'@'localhost' IDENTIFIED BY 'dev_password';
+
+GRANT ALL PRIVILEGES ON *.* TO 'dev_user'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+
